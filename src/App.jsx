535171931431c92bb227ee9e500f165708ec0b88
SamsonFlomo <flomosamson125@gmail.com>
2024-11-08 13:56:34 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Container from 'react-bootstrap/Container';
+
+
+function App() {
+
+  return (
+      <Container fluid id="App">
+        <h1>Meme Generator</h1> 
+      </Container>
+  )
+}
+
+export default App
